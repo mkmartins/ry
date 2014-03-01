8a95540c5244b334fcb67772b147b9ab0d1ed466
Marcos Martins <marcos.martins2307@gmail.com>
2014-03-01 07:21:20 -0700

    Adds initital gems to app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ry::Application.config.secret_token = '3ba26d91ac7f8cdee788a610b0cff718a7d487e8c52b408c591fbd6af0bb20939ec0c2501277f7afcc575879adbcdb494847cdde6821a777dd4771eb9aecf519'
